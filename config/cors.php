0e5b5b80165a316fe98a3787f00ee5ab7189fe36
qhuy2003 <hdnguyen107@gmail.com>
2026-03-30 23:56:33 +0700

    fix: add vercel domain to CORS

--- config/cors.php
+++ config/cors.php
@@ -1,34 +1,15 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | You may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may
-    | execute in web browsers. You are free to adjust these settings.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:5173'],
-
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://luanvan-gules.vercel.app',
+    ],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
-
 ];
